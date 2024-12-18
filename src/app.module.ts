434211059466557178d7eb5adef26b24eab65913
ajmalleonard <ajmalmaker@icloud.com>
2024-12-18 15:20:39 +0300

    refined schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PackagesModule } from './packages/packages.module';
+import { VisitsModule } from './visits/visits.module';
+import { PlacesModule } from './places/places.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
-  imports: [],
+  imports: [
+    PackagesModule,
+    VisitsModule,
+    PlacesModule,
+    BookingsModule,
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
