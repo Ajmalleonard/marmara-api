c8b0713627bb455ace0c615f8e77d6340da548e8
ajmaljs <ajmalmaker@icloud.com>
2025-01-28 02:00:39 +0300

    created new version of main point

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { AllExceptionsFilter } from './Guards/AllExceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 2020);
 }
 bootstrap();
+console.log('Server is running on http://localhost:2020');
