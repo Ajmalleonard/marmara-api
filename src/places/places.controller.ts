c17384ad17828335e48a8bec069fa2bd63e0239f
ajmaljs <ajmalmaker@icloud.com>
2025-02-01 18:46:21 +0300

    Update places.controller.ts

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ForbiddenException,
+} from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
 import { UpdatePlaceDto } from './dto/update-place.dto';
+import { Auth, GetUser } from '../decorators/Auth.decorator';
+import { User } from '@prisma/client';
 
 @Controller('places')
 export class PlacesController {
   constructor(private readonly placesService: PlacesService) {}
 
   @Post()
-  create(@Body() createPlaceDto: CreatePlaceDto) {
+  @Auth()
+  create(@Body() createPlaceDto: CreatePlaceDto, @GetUser() user: User) {
+    if (!user.isAdmin) {
+      throw new ForbiddenException('Only admins can create places');
+    }
     return this.placesService.create(createPlaceDto);
   }
 
   @Get()
-  findAll() {
-    return this.placesService.findAll();
+  findAll(@Query() query: any) {
+    return this.placesService.findAll(query);
+  }
+
+  @Get('top')
+  getTopPlaces() {
+    return this.placesService.getTopPlaces();
+  }
+
+  @Get('country/:country')
+  getByCountry(@Param('country') country: string) {
+    return this.placesService.getByCountry(country);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.placesService.findOne(+id);
+    return this.placesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
-    return this.placesService.update(+id, updatePlaceDto);
+  @Auth()
+  update(
+    @Param('id') id: string,
+    @Body() updatePlaceDto: UpdatePlaceDto,
+    @GetUser() user: User,
+  ) {
+    if (!user.isAdmin) {
+      throw new ForbiddenException('Only admins can update places');
+    }
+    return this.placesService.update(id, updatePlaceDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.placesService.remove(+id);
+  @Auth()
+  remove(@Param('id') id: string, @GetUser() user: User) {
+    if (!user.isAdmin) {
+      throw new ForbiddenException('Only admins can delete places');
+    }
+    return this.placesService.remove(id);
   }
 }
