4934ff0af9c3141a6d2c952aab468b6e4d9f761b
ajmaljs <ajmalmaker@icloud.com>
2025-02-01 18:46:10 +0300

    Create visit-query.dto.ts

---
+++ src/visits/dto/visit-query.dto.ts
@@ -0,0 +1,41 @@
+import { IsOptional, IsInt, Min, IsString, IsEnum } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export enum SortOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export class VisitQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  continent?: string;
+
+  @IsOptional()
+  @IsString()
+  startDate?: string;
+
+  @IsOptional()
+  @IsString()
+  endDate?: string;
+
+  @IsOptional()
+  @IsEnum(SortOrder)
+  sortOrder?: SortOrder = SortOrder.DESC;
+}
