d39671f8133b514113c825d3ff62bcbfab1a4e19
ajmaljs <ajmalmaker@icloud.com>
2025-02-13 04:39:19 +0300

    Update bookings.controller.ts

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,31 +1,31 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
-  UseGuards,
-  ForbiddenException,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBooking } from './dto/update-booking.dto';
-import { JwtAuthGuard } from '../guard/jwt-auth.guard';
 import { GetUser } from '../decorators/Auth.decorator';
 import { User } from '@prisma/client';
 
 @Controller('bookings')
-@UseGuards(JwtAuthGuard)
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
 
   @Post()
-  create(@Body() createBookingDto: CreateBookingDto, @GetUser() user: User) {
-    return this.bookingsService.create(createBookingDto, user.id);
+  create(@Body() createBookingDto: CreateBookingDto) {
+    console.log(createBookingDto);
+    console.log(createBookingDto.userId);
+    return this.bookingsService.create(
+      createBookingDto,
+      createBookingDto.userId,
+    );
   }
-
   @Get()
   findAll(@GetUser() user: User) {
     return this.bookingsService.findAll(user.id, user.isAdmin);
