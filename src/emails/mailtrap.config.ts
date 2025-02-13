157ae483d505b1fe4bbc82acec6136d27f8b8bcf
ajmaljs <ajmalmaker@icloud.com>
2025-02-13 04:39:36 +0300

    Update mailtrap.config.ts

--- src/emails/mailtrap.config.ts
+++ src/emails/mailtrap.config.ts
@@ -1,9 +1,11 @@
 import { MailtrapClient } from 'mailtrap';
 
-export const mailtrapClient = new MailtrapClient({
-  token: 'f5aec3a48f1d6b641bc18530f32d4032',
-});
+const TOKEN = 'f5aec3a48f1d6b641bc18530f32d4032';
+// const ENDPOINT = 'https://send.api.mailtrap.io/';
 
+export const mailtrapClient = new MailtrapClient({
+  token: TOKEN,
+});
 export const sender = {
   email: 'mailtrap@marmaraholidays.com',
   name: 'Marmaraholidays',
