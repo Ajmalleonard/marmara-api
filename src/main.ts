974a2704db53840007aa41a5cb482c0c14657b45
ajmaljs <ajmalmaker@icloud.com>
2025-10-20 14:57:29 +0300

    fix: change server port from 2121 to 2020

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
   // app.setGlobalPrefix('api');
 
   // Ensure the server is listening on all network interfaces
-  await app.listen(2121, '0.0.0.0');
+  await app.listen(2020, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
